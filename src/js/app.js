9a0c7a5ebbe83c5d9e93eeda5a61ac40311899e8
antonella <antonella_caroca@hotmail.com>
2018-08-11 17:50:07 -0400

    separación de responsabilidades

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,3 @@
-const hide = document.getElementById('hide');
-const show = document.getElementById('show');
-const tarea = document.getElementById('tarea');
-const tarea2 = document.getElementById('tarea2');
-const buttons = document.getElementById('buttons');
-const buttons2 = document.getElementById('buttons2');
-const btn = document.getElementById('btn');
-const btn2 = document.getElementById('btn2');
-const form2 = document.getElementById('container-2');
-
-hide.addEventListener('click', hideList);
-show.style.display = "none";
-tarea.style.display = "none";
-buttons.style.display = "none";
-
 function hideList(){
     hide.style.display = "none";
     show.style.display = "block";
@@ -23,17 +8,17 @@ function hideList(){
 function addTarea(){
     let el = document.createElement("div");
     el.innerHTML = "<textarea class='form-control form-control-sm' type='text' id='tarea' rows='2' cols='30' placeholder='añadir tarea'></textarea>";
-    document.getElementById("show").appendChild(el).focus(); 
+    show.appendChild(el).focus(); 
+    
 }
 
 function addTarea2(){
     let el2 = document.createElement("div");
     el2.innerHTML = "<textarea class='form-control form-control-sm' type='text' id='tarea2' rows='2' cols='30' placeholder='añadir tarea'></textarea>";
-    document.getElementById("show2").appendChild(el2).focus(); 
+    show2.appendChild(el2).focus(); 
 }
 
  btn.addEventListener('click', addForm);
-form2.style.display = "none";
 
 function addForm(){
 form2.style.display = "block"; 
---
+++ src/js/dom.js
@@ -0,0 +1,17 @@
+const hide = document.getElementById('hide');
+const show = document.getElementById('show');
+const show2 = document.getElementById('show2');
+const tarea = document.getElementById('tarea');
+const tarea2 = document.getElementById('tarea2');
+const buttons = document.getElementById('buttons');
+const buttons2 = document.getElementById('buttons2');
+const btn = document.getElementById('btn');
+const btn2 = document.getElementById('btn2');
+const form2 = document.getElementById('container-2');
+form2.style.display = "none";
+
+hide.addEventListener('click', hideList);
+show.style.display = "none";
+tarea.style.display = "none";
+buttons.style.display = "none";
+
