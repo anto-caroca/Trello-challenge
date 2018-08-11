2727fca2f0f47a3abbe956e0ad5f08555ef5575f
antonella <antonella_caroca@hotmail.com>
2018-08-11 16:57:29 -0400

    agrego form a la derecha

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,12 @@
 const hide = document.getElementById('hide');
 const show = document.getElementById('show');
 const tarea = document.getElementById('tarea');
+const tarea2 = document.getElementById('tarea2');
 const buttons = document.getElementById('buttons');
+const buttons2 = document.getElementById('buttons2');
+const btn = document.getElementById('btn');
+const btn2 = document.getElementById('btn2');
+const form2 = document.getElementById('container-2');
 
 hide.addEventListener('click', hideList);
 show.style.display = "none";
@@ -21,8 +26,26 @@ function addTarea(){
     document.getElementById("show").appendChild(el).focus(); 
 }
 
+function addTarea2(){
+    let el2 = document.createElement("div");
+    el2.innerHTML = "<textarea class='form-control form-control-sm' type='text' id='tarea2' rows='2' cols='30' placeholder='añadir tarea'></textarea>";
+    document.getElementById("show2").appendChild(el2).focus(); 
+}
+
+ btn.addEventListener('click', addForm);
+form2.style.display = "none";
+
+function addForm(){
+form2.style.display = "block"; 
+tarea2.style.display = "none";
+}
+
 function removeTarea(){
     let el = document.getElementById("show");
     el.removeChild(el.lastChild);
 }
 
+function removeTarea2(){
+    let el2 = document.getElementById("show2");
+    el2.removeChild(el2.lastChild);
+}
